0d4c6d41f772bc6b87062ef1b783826d94d7e4e9
[CCastillo] <christina.castillo04@utrgv.edu]>
2022-11-22 10:18:51 -0600

    Add game data project

---
+++ Games-data/games-over-70-rating-query.sql
@@ -0,0 +1,1 @@
+SELECT * FROM games WHERE user_rating_value >= 70 ORDER by user_rating_value DESC;
---
+++ Games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+--INSERT into sandbox(int16_value,string_value,datetime_value) VALUEs(2045,'numbers','1999-4-20 00:00:00');
+
+--UPDATE sandbox SET int32_value = 1029 WHERE id = 92;
+
+--DELETE from sandbox WHERE id = 96;
+
+SELECT * from sandbox;
